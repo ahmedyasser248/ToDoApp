b7155af44fe318a844816578fb4699fc1e9121f0
sarahelshabrawy <hylrcs@gmail.com>
2020-10-13 19:33:07 +0200

    MainActivity&Navigation

--- app/src/main/java/com/example/android/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/android/todoapp/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.example.android.todoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.android.todoapp.database.AppDatabase
+import com.example.android.todoapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var drawer:DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val toolbar: androidx.appcompat.widget.Toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        drawer = binding.drawerLayout
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        val bottomNav= binding.bottomNavigationView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.tasksFragment, R.id.tracker))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
         val dataSource = AppDatabase.getInstance(application).appDatabaseDao
-
-
+    }
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
